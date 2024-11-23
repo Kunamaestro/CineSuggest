0ca117ba6b260883810f8630ead6469ee70ecd43
henri <henri-pham@live.fr>
2024-11-23 19:58:50 +0100

    Header a peu pres fini

--- src/app/components/structural_sections/header/header.component.ts
+++ src/app/components/structural_sections/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatRippleModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
